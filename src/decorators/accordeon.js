d969e8bf198b1a00960a8f937fdc5e30cde66bfb
Roman <kaboom.ua@gmail.com>
2016-12-21 23:05:41 +0200

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function accordeon(Component) {
     return class WrapperComponent extends React.Component {
         state = {
+            //суть декораторов в переисползовании кода, не привязывайся к названиям сущностей. Лучше openItemId
             openArticleId: null
         }
 
@@ -12,6 +13,7 @@ export default function accordeon(Component) {
 
         toggleOpenArticle = id => ev => {
             ev && ev.preventDefault && ev.preventDefault()
+            //а закрывать уже открытую?
             this.setState({
                 openArticleId: id
             })
